632497fafa77495a9b2663a3299d4746292ae8ff
g-gandini <74453402+g-gandini@users.noreply.github.com>
2020-12-22 17:40:37 +0100

    Aggiunta gestione eccezioni MQTT

--- gw_subs_UDP.py
+++ gw_subs_UDP.py
@@ -1,22 +1,19 @@
 ########## Importazione moduli ##########
-import configparser
-# debug
-import sys
-
-print(sys.version_info)
-# debug
 import paho.mqtt.client as mqtt
 import socket
 import threading
+import configparser
+import logging
+
 
 ########## Parametri ##########
 config = configparser.ConfigParser()
 config.read('config.ini')
 port = config['RASPBERRY']['port']
-addr = config['RASPBERRY']['address']  # Cambiare con l'indirizzo IPv4 del proprio gateway (RB/PC)
+addr = config['RASPBERRY']['address']
 gateway = (addr, int(port))
-print(gateway)
 addr_broker = config['mqtt_broker']['address']
+port_broker = config['mqtt_broker']['port']
 topic = "test/topic"
 
 
@@ -31,13 +28,13 @@ class Queue:
         return self.elem != 0
 
     def inQueue(self, item):
-        self.items.insert(0, item)
-        if self.elem == 0:
+        self.items.insert(0,item)
+        if(self.elem == 0):
             self.head = item
         self.elem += 1
 
     def outQueue(self):
-        if (self.elem == 1):
+        if(self.elem == 1):
             self.head = None
         self.elem -= 1
         return self.items.pop()
@@ -45,18 +42,27 @@ class Queue:
 
 ########## on_connect() ##########
 def on_connect(pubsub, userdata, flags, rc):
-    print(f"Risultato connessione al broker con indirizzo IPv4 {addr_broker} (0 -> successo): [{rc}]\n")
-    pubsub.subscribe(topic)
-    print(f"Iscritto al topic: {topic}\n")
+    if(rc != 0):
+        logging.warning(f"Connessione al Broker con indirizzo IPv4 {addr_broker} fallita: codice [{rc}].\n")
+    else:
+        print(f"Connessione al Broker con indirizzo IPv4 {addr_broker} riuscita.\n")
+    try:
+        result, mid = pubsub.subscribe(topic)
+        if(result != 0):
+            logging.warning(f"Iscrizione al topic {topic} fallita: codice [{result}].\n")
+        else:
+            print(f"Iscrizione al topic {topic} riuscita.\n")
+    except Exception as err:
+        logging.error(f"Errore in iscrizione al topic {topic}:\n{err}\n")
 
 
 ########## on_disconnect() ##########
 def on_disconnect(pubsub, userdata, rc):
-    if (rc != 0):
-        print(f"Disconnessione inaspettata: codice [{rc}].\n")
+    if(rc != 0):
+        logging.warning(f"Disconnessione inaspettata dal Broker con indirizzo IPv4 {addr_broker}: codice [{rc}].\n")
     else:
-        print(f"Disconnessione dal broker con indirizzo IPv4 {addr_broker}.\n")
-
+        print(f"Disconnessione dal Broker con indirizzo IPv4 {addr_broker}.\n")
+        
 
 ########## on_message() ##########
 def on_message(pubsub, userdata, msg):
@@ -68,7 +74,7 @@ def on_message(pubsub, userdata, msg):
 def queue2broker(pubsub, q):
     pubsub.loop_start()
     while True:
-        if (q.notEmpty()):
+        if(q.notEmpty()):
             data = q.outQueue()
             pubsub.publish(topic, data)
     pubsub.loop_stop()
@@ -87,34 +93,36 @@ def initialize(gateway):
     # Server UDP
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except socket.error as e:
+    except socket.error as err:
         try:
             s.close()
         except Exception:
             pass
-        raise SystemError("Problema nella creazione della socket: 2*\n" + str(e))
-
+        raise SystemError(f"Errore in creazione socket:\n{err}\n")
     try:
-        # Complicato da testare...
         s.bind(gateway)
     except socket.error as e:
         try:
             s.close()
         except Exception:
             pass
-        raise SystemError("Problema nel bind della socket: 2*\n" + str(e))
-    pubsub = mqtt.Client(client_id="", clean_session=True, userdata=None)
+        raise SystemError(f"Errore in bind socket:\n{err}\n")
+    print("Server UDP inizializzato. In ascolto...\n")
+    # Client MQTT
+    pubsub = mqtt.Client(client_id = "", clean_session = True, userdata = None)
     pubsub.on_connect = on_connect
     pubsub.on_disconnect = on_disconnect
     pubsub.on_message = on_message
-    pubsub.connect(addr_broker, 1883, 60)
+    try:
+        pubsub.connect(addr_broker, int(port_broker), 60)
+    except Exception as err:
+        logging.error(f"Errore in connessione al Broker con indirizzo IPv4 {addr_broker}:\n{err}\n")
     # Avvio Gateway
     q = Queue()
-    thread_out = threading.Thread(target=queue2broker, args=(pubsub, q))
-    thread_in = threading.Thread(target=client2queue, args=(s, q))
+    thread_out = threading.Thread(target = queue2broker, args = (pubsub, q))
+    thread_in = threading.Thread(target = client2queue, args = (s, q))
     thread_out.start()
     thread_in.start()
-
 
 
 ########## Programma ##########
